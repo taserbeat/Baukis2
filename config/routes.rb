d358cbd76bebbd3493a7e40301424d44f9a32532
taserbeat <taserbeat@gmail.com>
2020-07-29 21:51:15 +0900

    ルーティングの制約を設定(Chapter 9まで完了)

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
-  namespace :staff, path: '' do
-    root 'top#index'
-    get 'login' => 'sessions#new', as: :login
-    resource :session, only: %i[create destroy]
-    resource :account, expect: %i[new create destroy]
+  config = Rails.application.config.baukis2
+
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path] do
+      root 'top#index'
+      get 'login' => 'sessions#new', as: :login
+      resource :session, only: %i[create destroy]
+      resource :account, expect: %i[new create destroy]
+    end
   end
 
-  namespace :admin do
-    root 'top#index'
-    get 'login' => 'sessions#new', as: :login
-    resource :session, only: %i[create destroy]
-    resources :staff_members
+  constraints host: config[:admin][:host] do
+    namespace :admin, path: config[:admin][:path] do
+      root 'top#index'
+      get 'login' => 'sessions#new', as: :login
+      resource :session, only: %i[create destroy]
+      resources :staff_members
+    end
   end
 
-  namespace :customer do
-    root 'top#index'
+  constraints host: config[:customer][:host] do
+    namespace :customer, path: config[:customer][:path] do
+      root 'top#index'
+    end
   end
 end
