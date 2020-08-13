2dde02db859fe0bf5daeaa356f719869afd40285
taserbeat <taserbeat@gmail.com>
2020-08-13 15:22:46 +0900

    アカウントの共通処理をモジュールとして共通化 (Chapter 17-3まで完了)

--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,21 +1,10 @@
 class StaffMember < ApplicationRecord
-  include StringNormalizer
+  include EmailHolder
+  include PersonalNameHolder
+  include PasswordHolder
 
   has_many :events, class_name: 'StaffEvent', dependent: :destroy
 
-  before_validation do
-    self.family_name = normalize_as_name(family_name)
-    self.given_name = normalize_as_name(given_name)
-    self.family_name_kana = normalize_as_furigana(family_name_kana)
-    self.given_name_kana = normalize_as_furigana(given_name_kana)
-    self.email = normalize_as_email(email)
-  end
-
-  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/.freeze
-  HUMAN_NAME_REGEXP = /\A[\p{han}\p{hiragana}\p{katakana}\u{30fc}A-Za-z]+\z/.freeze
-
-  validates :family_name, :given_name, presence: true, format: { with: HUMAN_NAME_REGEXP, allow_blank: true }
-  validates :family_name_kana, :given_name_kana, presence: true, format: { with: KATAKANA_REGEXP, allow_blank: true }
   validates :start_date, presence: true, date: {
     after_or_equal_to: Date.new(2000, 1, 1),
     before: ->(_obj) { 1.year.from_now.to_date },
@@ -26,15 +15,6 @@ class StaffMember < ApplicationRecord
     before: ->(_obj) { 1.year.from_now.to_date },
     allow_blank: true
   }
-  validates :email, presence: true, "valid_email_2/email": true, uniqueness: { case_sensitive: false }
-
-  def password=(raw_password)
-    if raw_password.is_a?(String)
-      self.hashed_password = BCrypt::Password.create(raw_password)
-    elsif raw_password.nil?
-      self.hashed_password = nil
-    end
-  end
 
   def active?
     !suspended? && start_date <= Date.today && (end_date.nil? || end_date > Date.today)
